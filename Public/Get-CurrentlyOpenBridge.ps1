8c4767566f584c8ff11eb0a759debc53f847906c
Jan Hoek <jhoek@uncommonsense.nl>
2023-11-02 19:27:31 +0100

    Formatting for currently open bridges; added BridgeName

--- Public/Get-CurrentlyOpenBridge.ps1
+++ Public/Get-CurrentlyOpenBridge.ps1
@@ -7,11 +7,9 @@ function Get-CurrentlyOpenBridge
     Invoke-WebRequest 'https://brugopen.nl'
     | Select-Object -ExpandProperty Content
     | pup '#openbridges li json{}' --plain
-    | jq '[ .[] | { Bridge: .children[0].href, SinceText: .children[1].text, Duration: .children[2].text }]'
+    | jq '[ .[] | { BridgeID: .children[0].href, BridgeName: .children[0].text, SinceText: .children[1].text, Duration: .children[2].text }]'
     | ConvertFrom-Json
     | ForEach-Object { $_ | Add-Member -NotePropertyName Since -NotePropertyValue (ConvertTo-DateTime -InputObject $_.SinceText) -PassThru }
-    | ForEach-Object { $_.Bridge = $_.Bridge -replace '^/', '' -replace '/$', ''; $_ }
+    | ForEach-Object { $_.BridgeID = $_.BridgeID -replace '^/', '' -replace '/$', ''; $_ }
     | ForEach-Object { $_.PSTypeNames.Insert(0, 'UncommonSense.BridgeOpenings.CurrentlyOpenBridge'); $_ }
 }
-
-# FIXME: Formatting file for this type
