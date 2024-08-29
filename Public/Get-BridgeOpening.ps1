12f5b3c343d826df91ad19062c198b93db719853
Jan Hoek <github@uncommonsense.nl>
2024-08-29 13:54:13 +0200

    Fixed pipeline support

--- Public/Get-BridgeOpening.ps1
+++ Public/Get-BridgeOpening.ps1
@@ -7,25 +7,28 @@ function Get-BridgeOpening
         [string[]]$Bridge
     )
 
-    $Bridge.ForEach{
-        $CurrentBridge = $_
-        $Url = "https://brugopen.nl/$CurrentBridge"
+    process
+    {
+        $Bridge.ForEach{
+            $CurrentBridge = $_
+            $Url = "https://brugopen.nl/$CurrentBridge"
 
-        ConvertTo-HtmlDocument -Uri $Url
-        | Select-HtmlNode -CssSelector '.lastoperations tbody tr' -All
-        | ForEach-Object {
-            $Th = $_ | Select-HtmlNode -CssSelector 'th' -All
-            $Td = $_ | Select-HtmlNode -CssSelector 'td' -All
+            ConvertTo-HtmlDocument -Uri $Url
+            | Select-HtmlNode -CssSelector '.lastoperations tbody tr' -All
+            | ForEach-Object {
+                $Th = $_ | Select-HtmlNode -CssSelector 'th' -All
+                $Td = $_ | Select-HtmlNode -CssSelector 'td' -All
 
-            [pscustomobject]@{
-                PSTypeName   = 'UncommonSense.BridgeOpenings.BridgeOpening'
-                Bridge       = $CurrentBridge
-                FromText     = $Th[0] | Get-HtmlNodeText
-                ToText       = $Th[1] | Get-HtmlNodeText
-                DurationText = $Td[0] | Get-HtmlNodeText
-                Reason       = $Td[1] | Get-HtmlNodeText
+                [pscustomobject]@{
+                    PSTypeName   = 'UncommonSense.BridgeOpenings.BridgeOpening'
+                    Bridge       = $CurrentBridge
+                    FromText     = $Th[0] | Get-HtmlNodeText
+                    ToText       = $Th[1] | Get-HtmlNodeText
+                    DurationText = $Td[0] | Get-HtmlNodeText
+                    Reason       = $Td[1] | Get-HtmlNodeText
+                }
             }
+            | ForEach-Object { $_ | Add-FromAndToDate }
         }
-        | ForEach-Object { $_ | Add-FromAndToDate }
     }
 }
